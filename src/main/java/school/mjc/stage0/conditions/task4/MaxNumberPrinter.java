5022a6b61d6b85ab87ca17bd90b92dabf5d90644
albert-pakichev <69592416+albert-pakichev@users.noreply.github.com>
2023-07-05 23:06:19 +0500

    Update MaxNumberPrinter.java

--- src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.conditions.task4;
 
 public class MaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        if((first>second && first>third) || (first==second) ||(first==third)) {
+            System.out.println(first);
+        }
+        else if((first<second && second>third) || (second==third)) {
+            System.out.println(second);
+        }
+        else if(first<third && second<third) {
+            System.out.println(third);
+        }
     }
 }
