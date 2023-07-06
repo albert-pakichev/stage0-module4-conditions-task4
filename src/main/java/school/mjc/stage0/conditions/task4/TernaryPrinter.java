260f2c9914bbf441d380efccbecc455d9201ff25
albert-pakichev <69592416+albert-pakichev@users.noreply.github.com>
2023-07-06 15:44:01 +0500

    Update TernaryPrinter.java

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryPrinter.java
@@ -2,6 +2,8 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryPrinter {
     public void printWhichIsBigger(int first, int second) {
-        System.out.println((first>second)? first : second);
+        int result = first>second ? first : second;
+        int di = result>10 ? result:-10;
+        System.out.println(di);
     }
 }
